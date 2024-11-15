f3d7b77e84236f228cd84dc44ec6669a4bc8aa28
velychkodiana <143785215+velychkodiana@users.noreply.github.com>
2024-11-15 09:34:47 +0200

    Update BeerSAXParser.java

--- src/main/java/org/example/beercatalog/BeerSAXParser.java
+++ src/main/java/org/example/beercatalog/BeerSAXParser.java
@@ -10,21 +10,29 @@ import java.io.File;
 
 public class BeerSAXParser {
 
+    // Метод для парсингу XML файлу за допомогою SAX парсера
     public void parseXML(String filePath) {
         try {
+            // Створюємо фабрику SAXParser
             SAXParserFactory factory = SAXParserFactory.newInstance();
+            // Створюємо SAXParser для розбору XML
             SAXParser saxParser = factory.newSAXParser();
 
+            // Створюємо обробник для обробки елементів XML
             BeerHandler handler = new BeerHandler();
+            // Парсимо файл за допомогою SAX парсера та нашого обробника
             saxParser.parse(new File(filePath), handler);
 
         } catch (Exception e) {
+            // якщо є помилки - виводимо їх
             e.printStackTrace();
         }
     }
 
+    // Внутрішній клас, який обробляє елементи XML за допомогою SAX
     private static class BeerHandler extends DefaultHandler {
 
+        // Логічні змінні для відстеження стану елементів XML
         private boolean isName = false;
         private boolean isType = false;
         private boolean isAlcohol = false;
@@ -32,8 +40,10 @@ public class BeerSAXParser {
         private boolean isIngredient = false;
         private boolean isChar = false;
 
+        // Метод, який викликається на кожен стартовий тег елемента
         @Override
         public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            // Перевіряємо, який елемент почався, і ставимо відповідну змінну в true
             switch (qName) {
                 case "Name" -> isName = true;
                 case "Type" -> isType = true;
@@ -44,19 +54,25 @@ public class BeerSAXParser {
             }
         }
 
+        // Метод, який викликається на кожен кінцевий тег елемента
         @Override
         public void endElement(String uri, String localName, String qName) throws SAXException {
+            // Якщо зустріли тег "Beer", виводимо повідомлення про завершення обробки пива
             if ("Beer".equals(qName)) {
                 System.out.println("End of Beer\n");
             }
         }
 
+        // Метод, який викликається, коли зустрічаються текстові дані між тегами
         @Override
         public void characters(char[] ch, int start, int length) throws SAXException {
+            // Отримуємо текстовий вміст між тегами
             String content = new String(ch, start, length).trim();
+
+            // В залежності від того, який елемент зараз обробляється, виводимо його значення
             if (isName) {
                 System.out.println("Name: " + content);
-                isName = false;
+                isName = false;  // Скидаємо прапор після обробки
             } else if (isType) {
                 System.out.println("Type: " + content);
                 isType = false;
@@ -76,8 +92,11 @@ public class BeerSAXParser {
         }
     }
 
+    // Метод main для запуску парсингу XML файлу
     public static void main(String[] args) {
+        // Створюємо об'єкт BeerSAXParser
         BeerSAXParser parser = new BeerSAXParser();
+        // Вказуємо шлях до XML файлу та викликаємо метод парсингу
         parser.parseXML("path/to/your/beer_catalog.xml");
     }
 }
